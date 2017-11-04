dd9a0a2ea0525d7709db8a082b56b5999052b9ca
Unknown <jack.yujia.yuan@gmail.com>
2017-11-04 10:38:24 +0800

    PipeServer_WinForm
    And there is a PipeClient_WinForm.

---
+++ PipeServer_WinForm/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PipeServer_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lbState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(13, 197);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(127, 13);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Enter your message here:";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(16, 218);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(363, 102);
+            this.tbInput.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(16, 339);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(363, 33);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.Location = new System.Drawing.Point(16, 13);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(234, 40);
+            this.lbInfo.TabIndex = 3;
+            this.lbInfo.Click += new System.EventHandler(this.lbInfo_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(19, 129);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(360, 65);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Click to connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lbState
+            // 
+            this.lbState.Location = new System.Drawing.Point(16, 53);
+            this.lbState.Name = "lbState";
+            this.lbState.Size = new System.Drawing.Size(234, 73);
+            this.lbState.TabIndex = 5;
+            this.lbState.Click += new System.EventHandler(this.lbState_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 401);
+            this.Controls.Add(this.lbState);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "PipeServer_WinForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lbState;
+    }
+}
+
---
+++ PipeServer_WinForm/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.IO;
+
+namespace PipeServer_WinForm
+{
+    public partial class Form1 : Form
+    {
+        NamedPipeServerStream pipeServer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lbInfo.Text = "This is server side.\nClick Connect to set up connection";
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Read one line of user input from the console and sends that to the client process.
+                using (StreamWriter sw = new StreamWriter(pipeServer))
+                {
+                    sw.AutoFlush = true;
+                    sw.WriteLine(tbInput.Text.ToString());
+                }
+            }
+            // This server program finishes after it has written the text line to the named pipe.
+            catch (IOException ex) // Catch the IOException that is raised if the pipe is broken or disconnected.
+            {
+                lbInfo.Text += "ERROR: " + ex.Message;
+            }
+
+
+
+            lbState.Text = "Message sent. If you want to send again please click \"Connect\" again.";
+            btnSend.Enabled = false;
+
+        }
+
+        private void lbInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (pipeServer != null)
+            {
+                pipeServer.Close();
+            }
+            pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.Out);
+            lbState.Text += "\nNamedPipeServerStream object has been created.";
+
+            lbState.Text = "\nWaiting for client connection...";
+            // Wait for a client to connect
+
+            pipeServer.WaitForConnection();
+
+            if (pipeServer.IsConnected)
+            {
+                lbState.Text += "\nClient connected.";
+                lbState.Text += "\nEnter text.";
+            }
+
+
+            btnSend.Enabled = true;
+
+
+
+
+
+        }
+
+        private void lbState_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
